810e51d9cdd59c2017bb777226776411edc103aa
Derek Rush <drush81@gmail.com>
2019-05-06 18:37:58 -0700

    init commit

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :cffd_live, CffdWeb.Endpoint,
   load_from_system_env: true,
   http: [port: System.get_env("PORT")],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
+  url: [scheme: "https", host: "cffd-live", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
