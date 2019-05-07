aaffbfe4a86bd193500cebc38840d38a8ae262b1
Derek Rush <drush81@gmail.com>
2019-05-06 17:13:22 -0700

    init commit

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,9 @@ config :cffd_live, :lib_path, "/tmp/build/lib"
 
 config :cffd_live, CffdWeb.Endpoint,
   load_from_system_env: true,
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  url: [scheme: "https", host: "cffd-live.herokuapp.com", port: 443],
+  http: [port: System.get_env("PORT")],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
